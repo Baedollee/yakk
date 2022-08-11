be9b8c0d61f8cd5928998120a3a55e9806cb6945
raaang <graytorgb@gmail.com>
2022-08-11 15:42:53 +0900

    heroku devTools 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -6,8 +6,8 @@ const store = configureStore({
   reducer: {
     reply: replySlice,
     comment: postSlice,
-		devTools: process.env.NODE_ENV !== 'production',
   },
+	devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
